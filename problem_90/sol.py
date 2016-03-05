89571d61ebd73102d62a8718ef75138c12ede748
Danny Ye <dannyxx001@gmail.com>
2016-03-06 00:46:20 +0800

    Update sol.py

--- problem_90/sol.py
+++ problem_90/sol.py
@@ -14,4 +14,4 @@ class Solution(object):
         for i in xrange(index,len(nums)):
             if(i != index and nums[i] == nums[i-1]):
                 continue
-        self.dfs(i+1,subset+[nums[i]],nums,result)
+            self.dfs(i+1,subset+[nums[i]],nums,result)
