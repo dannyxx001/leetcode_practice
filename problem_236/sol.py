ecf5f957e86cfa37515b4c1edda11d1617006a87
danny ye <dannyxx001@gmail.com>
2016-03-23 23:57:33 +0800

    unsolve problem 236

---
+++ problem_236/sol.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from Queue import Queue
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        p_found, q_found = 0, 0
+        layer = 0
+        parent_map = []
+        bfs_queue = Queue()
+        bfs_queue.put(root)
+        while not (p_found and q_found):
+            layer += 1
+            current = bfs_queue.get()
+            parent_map.append(current)
+            
+            if current == p:
+                p_found = layer
+            elif current == q:
+                q_found = layer
+            
+            if current.left != None:
+                bfs_queue.put(current.left)
+            if current.right != None:
+                bfs_queue.put(current.right)
+                
+        print p_found,q_found
+        
+        while p_found != q_found:
+            if p_found > q_found:
+                p_found //= 2
+            else:
+                q_found //= 2
+        
+        return parent_map[p_found-1]
